e2ae7047a125ad16642df0d78eaa0720a2e0ea7e
Davidbdry <davidbdry@gmail.com>
2025-04-27 13:05:07 +0200

    fix(asset): removing react asset

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import path from 'node:path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      ...(process.env.VITE_APP_ENV === 'development' && {
+        proxy: {
+          '/api': {
+            target: 'https://v2.demo.sylius.com',
+            changeOrigin: true,
+            rewrite: (path) => path,
+          },
+        },
+      }),
+    },
+  };
+});
